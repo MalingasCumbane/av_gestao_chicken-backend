a5e9a46cace9469073d103f5eb3286f0b8075f16
MalingasCumbane <malingascumbane1@gmail.com>
2026-04-21 11:55:19 +0200

    Upda details on it

--- avgestao_backend/settings.py
+++ avgestao_backend/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'avgestao_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "avi_gestao",
-        "USER": "postgres",
-        "PASSWORD": "Wsxhfs@@12ey",
-        "HOST": "localhost",
-        "PORT": "5432"
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#      "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "avi_gestao",
+#         "USER": "postgres",
+#         "PASSWORD": "Wsxhfs@@12ey",
+#         "HOST": "localhost",
+#         "PORT": "5432"
+#      }
+# }
  
 # DATABASES = {
 #    'default': {
